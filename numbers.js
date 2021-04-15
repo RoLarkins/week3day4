80b149402d01be02f6e6063ca86766c7694e583c
Roosevelt <LarkinsRo@gmail.com>
2021-04-15 05:28:13 -0700

    work

---
+++ numbers.js
@@ -0,0 +1,35 @@
+var a = 5;
+var b = 6;
+var c = 7;
+var d = 8; 
+var e = 9;
+var f = 10;
+var g = 11;
+var h = 12; 
+var i = 13;
+var j = 14; 
+
+
+console.log(b+j);
+console.log(c+e);
+
+
+console.log(b-a);
+console.log(c-b);
+
+
+console.log(b*c);
+console.log(e*d);
+
+
+console.log(e/a);
+console.log(h/b);
+
+
+
+console.log(++a);
+console.log(++i);
+
+
+console.log(--g);
+console.log(--d);
